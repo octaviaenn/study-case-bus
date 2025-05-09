82e34ae637ebf49ed6176086280daada27be3974
octaviaenn <octaviaenn@gmail.com>
2025-05-09 18:36:20 +0700

    fix harga

--- src/Route.java
+++ src/Route.java
@@ -7,21 +7,24 @@ import java.io.*;
 public class Route extends JPanel {
     private static JLabel hargaTiket;
 
+
     public Route(JFrame frame) {
         setLayout(new FlowLayout(FlowLayout.LEFT));
         setPreferredSize(new Dimension(400, 250));
         // setLayout(new FlowLayout(FlowLayout.LEFT));
         // setPreferredSize(new Dimension(300, 700));
 
+        String[] data = {
+                "Wilangan", "Ngawi", "Gendingan", "Solo", "Kartosuro", "Jogja", "Magelang"
+        };
+
         JLabel inputLokasiAwal = new JLabel("Pilih lokasi awal anda: ");
-        JComboBox<String> lokasiAwal = new JComboBox<>(
-                new String[]{"Wilangan", "Ngawi", "Gendingan", "Solo", "Kartosuro", "Jogja"});
+        JComboBox<String> lokasiAwal = new JComboBox<>(data);
         // lokasiAwal.setPreferredSize(new Dimension(10, 30));
         lokasiAwal.setSelectedIndex(0);
 
         JLabel inputLokasiAkhir = new JLabel("Pilih lokasi tujuan anda: ");
-        JComboBox<String> lokasiAkhir = new JComboBox<>(
-                new String[]{"Ngawi", "Gendingan", "Solo", "Kartosuro", "Jogja", "Magelang"});
+        JComboBox<String> lokasiAkhir = new JComboBox<>(data);
         // lokasiAkhir.setPreferredSize(new Dimension(10, 30));
 
         lokasiAkhir.setSelectedIndex(0);
@@ -29,13 +32,13 @@ public class Route extends JPanel {
         // Map<String, String> lokasi = new HashMap<>();
         String invalid = "Rute bus tidak valid";
         String[][] harga = {
-                {invalid, invalid, invalid, invalid, invalid, invalid, invalid},
                 {invalid, "Rp 35.000", "Rp 45.000", "Rp 55.000", "Rp 60.000", "Rp 70.000", "Rp 85.000"},
-                {invalid, invalid, "Rp 25.000", "Rp 30.000", "Rp 40.000", "Rp 45.000", "Rp 60.000"},
-                {invalid, invalid, invalid, "Rp 25.000", "Rp 35.000", "Rp 40.000", "Rp 55.000"},
-                {invalid, invalid, invalid, invalid, "Rp 15.000", "Rp 15.000", "Rp 30.000"},
-                {invalid, invalid, invalid, invalid, invalid, "Rp 15.000", "Rp 30.000"},
-                {invalid, invalid, invalid, invalid, invalid, invalid, "Rp 15.000"},
+                {"Rp 35.000", invalid, "Rp 25.000", "Rp 30.000", "Rp 40.000", "Rp 45.000", "Rp 60.000"},
+                {"Rp 45.000", "Rp 25.000", invalid, "Rp 25.000", "Rp 35.000", "Rp 40.000", "Rp 55.000"},
+                {"Rp 55.000", "Rp 30.000", "Rp 25.000", invalid, "Rp 15.000", "Rp 15.000", "Rp 30.000"},
+                {"Rp 60.000", "Rp 40.000", "Rp 35.000", "Rp 15.000", invalid, "Rp 15.000", "Rp 30.000"},
+                {"Rp 70.000", "Rp 45.000", "Rp 40.000", "Rp 15.000", "Rp 15.000", invalid, "Rp 15.000"},
+                {"Rp 85.000", "Rp 60.000", "Rp 55.000", "Rp 30.000", "Rp 30.000", "Rp 15.000", invalid}
         };
 
         add(inputLokasiAwal);
@@ -58,15 +61,17 @@ public class Route extends JPanel {
                     String selectedItem = (String) lokasiAwal.getSelectedItem();
                     String selectedItem2 = (String) lokasiAkhir.getSelectedItem();
                     int index = switch (selectedItem) {
-                        case "Wilangan" -> 1;
-                        case "Ngawi" -> 2;
-                        case "Gendingan" -> 3;
-                        case "Solo" -> 4;
-                        case "Kartosuro" -> 5;
-                        case "Jogja" -> 6;
+                        case "Wilangan" -> 0;
+                        case "Ngawi" -> 1;
+                        case "Gendingan" -> 2;
+                        case "Solo" -> 3;
+                        case "Kartosuro" -> 4;
+                        case "Jogja" -> 5;
+                        case "Magelang" -> 6;
                         default -> 0;
                     };
                     int index2 = switch (selectedItem2) {
+                        case "Wilangan" -> 0;
                         case "Ngawi" -> 1;
                         case "Gendingan" -> 2;
                         case "Solo" -> 3;
